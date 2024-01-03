da2ec78b978e0d50d569643a69be6a73080c20fc
Tamal Majumdar <125782688+Tom-1508@users.noreply.github.com>
2024-01-03 20:05:31 +0000

    updated

--- 01_basics/operators.py
+++ 01_basics/operators.py
@@ -1,2 +1,35 @@
 num1 = 10
 num2 = 4
+
+#Arithmetic Operators
+print("num1 + num2 is ", num1+num2)
+print("num1 - num2 is ", num1-num2)
+print("num1 * num2 is ", num1*num2)
+print("num1 / num2 is ", num1/num2)
+print("num1 // num2 is ", num1//num2)
+print("num1 ** num2 is ", num1**num2)
+print("num1 % num2 is ", num1%num2)
+
+
+#Assignment operators
+a = 4
+# a += 2
+a**=3
+print(a)
+
+
+# Comparison Operators
+x = 8
+y = 5
+print(x>y)
+print(x<y)
+print(x!=y)
+z = 8
+print(x==z)
+
+
+# Logical Operators
+print( x== z and x<y )
+print (x==z or x<y)
+print(not(False))
+print(not(True))
---
+++ 01_basics/strings.py
@@ -0,0 +1,18 @@
+name = 'tamal'
+number = "7"
+print (name)
+
+print(name[0:3]) # start from 0 and go all the way till 2(3-1)
+# print (name[a:b]) # start from a and go all the way till (b-1)
+print (name[1:4])
+
+# some String Methods
+print(name.upper())
+print (name.capitalize())
+print(name.count("a"))
+print (name.isalnum())
+print(number.isnumeric())
+print(name.removeprefix())
+
+
+# Tip : learn about string methods while doing projects . ( source : Python Docs )
