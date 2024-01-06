02a1e0e381a01ab2c88fb9808cba3be4d0ce51b6
Tamal Majumdar <tamalcrj2002@gmail.com>
2024-01-06 14:01:20 +0000

    updated

---
+++ 01_basics/functions.py
@@ -0,0 +1,17 @@
+def greetHello(name,ending):
+    print("hello"+ name)
+    print(ending)
+print("executing functions")
+greetHello("Ronit","Thanks")
+greetHello("Tamal","best")
+print("done")
+
+
+def letterGenerator(name, date):
+    st = f"hello maam,\nmyself {name}.I will not come on {date}"
+    print(st)
+letterGenerator("Ronit","23rd january")
+
+def avg (a,b):
+    return (a+b)/2
+print(avg(5,5))
