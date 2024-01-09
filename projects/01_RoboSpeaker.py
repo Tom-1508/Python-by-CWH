408f6a01cc08750513a81951957b97c2aa8b89a4
Tom-1508 <tamalcrj2002@gmail.com>
2024-01-09 15:43:45 +0530

    updated

--- projects/01_RoboSpeaker.py
+++ projects/01_RoboSpeaker.py
@@ -1,7 +1,15 @@
-import os
+import win32com.client as wincom
 
-if __name__ == '__main__':
-    print("Welcome to RoboSpeaker 1.0// Created by Tamal")
-    x = input("tell me what you want me to speak: ")
-    command = f"say {x}"
-    os.system(command)
+# you can insert gaps in the narration by adding sleep calls
+import time
+
+speak = wincom.Dispatch("SAPI.SpVoice")
+
+text = "hi souvik. what are you doing"
+speak.Speak(text)
+
+# 3 second sleep
+time.sleep(3) 
+
+text = "This text is read after 3 seconds"
+speak.Speak(text)
